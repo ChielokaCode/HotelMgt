b654c285f5f0eb20a2387671b530176f12a9d738
ChielokaCode <realmatec01@gmail.com>
2023-10-12 21:26:22 -0700

    updated main
    added a motto print statement

---
+++ src/main/java/org/hotelmgt/Main.java
@@ -0,0 +1,9 @@
+package org.hotelmgt;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Management System");
+        System.out.println("Book a Reservation from the comfort of your homes");
+    }
+}
